7998d3f04e2a978b058dea8da838dfe0f13ec0ab
arminkianian <arminkia.dev@gmail.com>
2024-03-20 12:09:41 +0330

    Testing - Integration Test

---
+++ Testing/test/IntegrationTests/ApplicationService/People/CreateNewPersonCommandHandlerTests.cs
@@ -0,0 +1,47 @@
+﻿using ApplicationService.People.CommandHandlers;
+using ApplicationService.People.Commands;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Zamin.Core.Domain.ValueObjects;
+
+namespace IntegrationTests.ApplicationService.People
+{
+    public class CreateNewPersonCommandHandlerTests : IClassFixture<PersonCommandRepositoryFixture>
+    {
+        private readonly PersonCommandRepositoryFixture _personCommandRepositoryFixture;
+
+        public CreateNewPersonCommandHandlerTests(PersonCommandRepositoryFixture personCommandRepositoryFixture)
+        {
+            _personCommandRepositoryFixture = personCommandRepositoryFixture;
+        }
+
+        [Fact]
+        public void when_pass_valid_input_value_expect_person_create_and_register_in_database()
+        {
+            //Arrange
+            var command = new CreateNewPersonCommand
+            {
+                Id = Guid.NewGuid(),
+                FirstName = "Armin",
+                LastName = "Kianian"
+            };
+
+            var commandHandler = new CreateNewPersonCommandHandler(_personCommandRepositoryFixture.PersonCommandRepository);
+
+            //Act
+            commandHandler.Handle(command);
+
+            //Assert
+            var person= _personCommandRepositoryFixture.Context.People.FirstOrDefault(p => p.BusinessId == BusinessId.FromGuid(command.Id));
+
+            person.ShouldNotBeNull();
+            person.FirstName.ShouldBe(command.FirstName);
+            person.LastName.ShouldBe(command.LastName);
+        }
+    }
+}
---
+++ Testing/test/IntegrationTests/ApplicationService/People/PersonCommandRepositoryFixture.cs
@@ -0,0 +1,28 @@
+﻿using Domain.People.Repositories;
+using Infrastructure;
+using Infrastructure.People;
+using Microsoft.EntityFrameworkCore;
+
+namespace IntegrationTests.ApplicationService.People
+{
+    public class PersonCommandRepositoryFixture : IDisposable
+    {
+        public IPersonCommandRepository PersonCommandRepository { get; }
+        public AppDbContext Context { get; }
+
+        public PersonCommandRepositoryFixture()
+        {
+
+            DbContextOptionsBuilder<AppDbContext> builder = new DbContextOptionsBuilder<AppDbContext>();
+            builder.UseSqlServer("server=.,1433; database=PeopleIntegrationTest; user id=sa; password=Abcd_1234; MultipleActiveResultSets=True; TrustServerCertificate=True;");
+            Context = new AppDbContext(builder.Options);
+            Context.Database.EnsureCreated();
+            PersonCommandRepository = new PersonCommandRepository(Context);
+        }
+
+        public void Dispose()
+        {
+            Context.Database.EnsureDeleted();
+        }
+    }
+}
